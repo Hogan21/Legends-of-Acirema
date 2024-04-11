2f298fe33f1119bd8f3974696887ab4e75073678
Supernamo06 <supernamo06@gmail.com>
2024-04-11 14:49:54 -0400

    HE JUMPS NOW

--- Legends of Acirema/Assets/Scripts/MoveingThePlayer.cs
+++ Legends of Acirema/Assets/Scripts/MoveingThePlayer.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class MoveingThePlayer : MonoBehaviour
 {
+    [SerializeField] private float speed = 5.0f;
+    private float verticalinput;
+    private float horizontalinput;
+    [SerializeField] private float sensitivity = 10f;
+
+    private Rigidbody playerRb;
+    [SerializeField] private bool isOnGround = true;
+    [SerializeField] private float jumpForce = 7;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRb = GetComponent<Rigidbody>();
     }
-
-    private float speed = 5.0f;
-    private float verticalinput;
-    private float horizontalinput;
-
-    public float sensitivity = 10f;
 
     // Update is called once per frame
     void Update()
@@ -26,6 +29,27 @@ public class MoveingThePlayer : MonoBehaviour
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalinput);
 
         transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
-        transform.Rotate(0, 0, -Input.GetAxis("QandE") * 90 * Time.deltaTime);
+        // transform.Rotate(0, 0, -Input.GetAxis("QandE") * 90 * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = false;
+        }
     }
 }
