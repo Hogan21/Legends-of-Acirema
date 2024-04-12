e3e425dd9879ff89ebab9be1d8862e080de8d350
Jordan J.  Taylor <144865@paulding.k12.ga.us>
2024-04-12 14:25:30 -0400

    u65

--- Legends of Acirema/Assets/Scripts/MoveingThePlayer.cs
+++ Legends of Acirema/Assets/Scripts/MoveingThePlayer.cs
@@ -1,27 +1,41 @@
+using Palmmedia.ReportGenerator.Core.Reporting.Builders;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveingThePlayer : MonoBehaviour
 {
+    [SerializeField] private float speed = 5.0f;
+    private float verticalinput;
+    private float horizontalinput;
+    private Rigidbody rb;
+    [SerializeField] private int force;
+
+
+    public float sensitivity = 10f;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
-    private float speed = 5.0f;
-    private float verticalinput;
-    private float horizontalinput;
-
-    public float sensitivity = 10f;
 
     // Update is called once per frame
     void Update()
     {
         verticalinput = Input.GetAxis("Vertical");
         horizontalinput = Input.GetAxis("Horizontal");
-        //move
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up) * force;
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            speed = 100.0f;
+        }
+
+
+        //moveNOW
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalinput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalinput);
 
